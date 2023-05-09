790eb480c307f3b5e2147aae26a206474f716c42
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-05-09 10:19:19 +0200

    commit

--- indx.js
+++ indx.js
@@ -18,22 +18,40 @@ if (ge < 22) {
     console.log("Welcome!")
 }*/
 
-let firstCard = 10
-let secondCard = 7
+let firstCard = 6
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById('cards-el')
 
-if (sum <= 20) {
-    message = "Do you want to draw new card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack"
-    hasBlackjack = true
-} else {
-    message
-    isAlive
+function startGame() {
+    function renderGame() {
+        cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+        sumEl.textContent = "Sum: " + sum
+
+        if (sum <= 20) {
+            message = "Do you want to draw new card?"
+        } else if (sum === 21) {
+            message = "Wohoo! You've got Blackjack"
+            hasBlackjack = true
+        } else {
+            message = "You're out of the game!"
+            isAlive = false
+        }
+        messageEl.textContent = message
+    }
 }
 
-console.log(message)
+
+
+function newCard() {
+    console.log("Drawing a new card from the deck")
+    let card = 6
+    sum += card
+    renderGame()
+}
